64aa03b61d82fa0d4e400278dd2315b6f82c0d7f
Gomathi sankar <104767307+TheSankar@users.noreply.github.com>
2026-03-05 19:21:00 +0530

    updated code

--- bot.py
+++ bot.py
@@ -1,33 +1,58 @@
 import requests
 import time
+import os
 
-TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "-100XXXXXXXX"
+TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message_id = None
 
 def get_price():
     url = "https://api.coingecko.com/api/v3/simple/price?ids=litentry&vs_currencies=usd&include_24hr_change=true"
     data = requests.get(url).json()
+
     price = data["litentry"]["usd"]
     change = data["litentry"]["usd_24h_change"]
+
     return price, change
 
+
 def send_message(text):
+    global message_id
+
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    r = requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": text
+    }).json()
+
+    message_id = r["result"]["message_id"]
+
+
+def edit_message(text):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/editMessageText"
+
     requests.post(url, data={
         "chat_id": CHAT_ID,
+        "message_id": message_id,
         "text": text
     })
 
+
 while True:
+
     price, change = get_price()
 
-    message = f"""
-LIT price / Lighter price
+    text = f"${price:.3f} • 24h: {change:.2f}%"
 
-${price}
+    if message_id is None:
+        send_message(text)
+    else:
+        edit_message(text)
 
-24h change: {round(change,2)}%
-"""
+    time.sleep(5)
 
     send_message(message)
 
